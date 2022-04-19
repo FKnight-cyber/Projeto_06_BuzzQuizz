309779969e18f0705094eea7ddef9c25c52ea231
FKnight-cyber <ryannicholas.vieira@gmail.com>
2022-04-19 13:24:02 -0300

    desktop 1 and 2 layout implemented, get quizzes from server implemented

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const API = "https://mock-api.driven.com.br/api/v6/buzzquizz";
+const quizzes = []
+
+obterQuizzes();
+
+function obterQuizzes(){
+    alert("OPA")
+    axios.get(`${API}/quizzes`).then(armazenarQuizzes);
+}
+
+function armazenarQuizzes(response){
+    quizzes = response.data;
+    renderizarQuizzes();
+}
+
+function renderizarQuizzes(){
+    
+    const ulQuizz = document.querySelector(".quizzContainer");
+
+    ulQuizz.innerHTML = "";
+
+    for(let i = 0;i < quizzes.length;i++){
+        ulQuizz.innerHTML += `
+        <li class="quizz">
+            <img src="${quizzes[i].image}" alt="" srcset="">
+            <div class="quizzTitle"><h2>${quizzes[i].title}</h2></div>
+        </li>
+        `
+    }
+}
