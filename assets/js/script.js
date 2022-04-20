458872e1bba48add119cd4ebf588d6aba28755ec
FKnight-cyber <ryannicholas.vieira@gmail.com>
2022-04-20 16:28:49 -0300

    criação de quizz

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,51 @@ function renderizarQuizzes(){
     }
 }
 
-function selectQuizz(elemento){
+function toLimit(string = ""){
+    string.value = string.value.substring(0,65);
+}
+
+function criarQuizz(){
+    document.querySelector(".maincontent").classList.add("hidden");
+}
+
+function isImage(url) {
+    return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
+  }
+
+function enviarInfBasicas(){
+    const title = document.querySelector(".title").value
+    const qtdPerguntas = document.querySelector(".qtdPerguntas").value
+    const qtdNiveis = document.querySelector(".qtdNivel").value
+    const image = document.querySelector(".quizzImgURL").value
+
+    if(isImage(image)){
+        if(title.length < 20 || qtdPerguntas < 3 || qtdNiveis < 2){
+            alert("Preencha os dados corretamente");
+        }
+    }
+
+    criarPerguntas();
+}
+
+function criarPerguntas(){
+    document.querySelector(".inputs").classList.add("hidden");
+
+    const textoPergunta = document.querySelector(".textoPergunta.nao-preenchida").value;
+    const respostaCorreta = document.querySelector(".respostaCorreta.nao-preenchida").value;
+    const imageC = document.querySelector(".imagemCorreta.nao-preenchida").value
+    const imageE = document.querySelector(".imagemErrada.nao-preenchida").value
+    const respostaIncorreta = document.querySelector(".respostaIncorreta.nao-preenchida").value;
+
+    if(textoPergunta.length < 20 || respostaCorreta === '' || !isImage(imageC)
+    || respostaIncorreta === '' || !isImage(imageE)){
+        alert("Preencha os dados corretamente");
+    } 
     
+    testar();
+
+}
+
+function testar(){
+    console.log(respostaIncorreta)
 }
