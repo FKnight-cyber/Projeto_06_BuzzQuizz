ad8bc689251d2ded04d5da6f71be8ec8b5b3aff3
FKnight-cyber <ryannicholas.vieira@gmail.com>
2022-04-20 23:43:32 -0300

    implementada criação de perguntas do quizz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 const API = "https://mock-api.driven.com.br/api/v6/buzzquizz";
+
+let totalDePerguntas = 0;
 let quizzes = []
 let myQuizz = {
 	id: 1,
@@ -109,52 +111,129 @@ function isImage(url) {
   }
 
 function enviarInfBasicas(){
-	document.querySelector(".pagequizz_1").classList.add("hidden")
-	document.querySelector(".pagequizz_2").classList.remove("hidden")
-
+	
     const title = document.querySelector(".title").value
-    const qtdPerguntas = document.querySelector(".qtdPerguntas").value
     const qtdNiveis = document.querySelector(".qtdNivel").value
     const image = document.querySelector(".quizzImgURL").value
+	const qtdPerguntas = document.querySelector(".qtdPerguntas").value;
 
     if(title.length < 20 || qtdPerguntas < 3 || qtdNiveis < 2 || !isImage(image)){
         alert("Preencha os dados corretamente");
-    }
-    
-    myQuizz.title = title;
-    myQuizz.image = image;
+    }else{
+		totalDePerguntas = qtdPerguntas;
+		document.querySelector(".pagequizz_1").classList.toggle("hidden");
+		document.querySelector(".pagequizz_2").classList.toggle("hidden");
 
-	const listaPerguntas = document.querySelector(".inputs_2");
+		myQuizz.title = title;
+    	myQuizz.image = image;
 
-	for(let i = 1;i < qtdPerguntas;i++){
-		listaPerguntas.innerHTML += `
-		<article class="newinputs">
-                <h4>Pergunta ${i+1}</h4>
-                <ion-icon name="brush-outline"></ion-icon>
+		const listaPerguntas = document.querySelector(".inputs_2");
+		const listaNiveis = document.querySelector(".inputs_3");
+
+		for(let i = 0;i < qtdPerguntas;i++){
+			listaPerguntas.innerHTML += `
+			<article class="newinputs">
+                <div class="content">
+                <div class="testando">
+                    <h4>Pergunta ${i+1}</h4>
+                    <ion-icon onclick="select(this)" name="brush-outline"></ion-icon>
+                </div>
+                <div class="testando2">
+                    <h4>Pergunta ${i+1}</h4>
+                    <input class="textoPergunta" type="text" placeholder="Texto da pergunta">
+                    <input class="corPergunta" type="text" pattern="^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$" placeholder="Cor de fundo da pergunta">
+                    <h4>Resposta Correta</h4>
+                    <input class="respostaCorreta" type="text" placeholder="Resposta Correta">
+                    <input class="imagemCorreta" type="text" placeholder="URL da imagem">
+                    <h4>Respostas incorretas</h4>
+                    <input class="respostaIncorreta1" type="text" placeholder="Resposta incorreta 1">
+                    <input class="imagemErrada1" type="text" placeholder="URL da imagem 1">
+            
+                    <input class="respostaIncorreta2" type="text" placeholder="Resposta incorreta 2">
+                    <input class="imagemErrada2" type="text" placeholder="URL da imagem 2">
+                            
+                    <input class="respostaIncorreta3" type="text" placeholder="Resposta incorreta 3">
+                    <input class="imagemErrada3" type="text" placeholder="URL da imagem 3">
+                </div>
+            	</div>  
             </article>
-	`
+			`
+		}
+
+		for(let i = 1;i < qtdNiveis;i++){
+			listaNiveis.innerHTML += `
+			<article class="newinputs">
+                	<h4>Nível ${i+1}</h4>
+               	 	<ion-icon name="brush-outline"></ion-icon>
+           	</article>
+			`
+		}
 	}
-    criarPerguntas();
+    
+    
 }
 
 function criarPerguntas(){
-    document.querySelector(".inputs").classList.add("hidden");
+	let conditionsNotMet = false;
+    const textoPergunta = document.querySelectorAll(".textoPergunta");
+    const respostaCorreta = document.querySelectorAll(".respostaCorreta");
+	const corPergunta = document.querySelectorAll(".corPergunta");
+    const imageC = document.querySelectorAll(".imagemCorreta");
+    const respostaIncorreta1 = document.querySelectorAll(".respostaIncorreta1");
+	const imageE1 = document.querySelectorAll(".imagemErrada1");
+	const respostaIncorreta2 = document.querySelectorAll(".respostaIncorreta2");
+	const imageE2 = document.querySelectorAll(".imagemErrada2");
+	const respostaIncorreta3 = document.querySelectorAll(".respostaIncorreta3");
+	const imageE3 = document.querySelectorAll(".imagemErrada3");
 
-    const textoPergunta = document.querySelector(".textoPergunta.nao-preenchida").value;
-    const respostaCorreta = document.querySelector(".respostaCorreta.nao-preenchida").value;
-    const imageC = document.querySelector(".imagemCorreta.nao-preenchida").value
-    const imageE = document.querySelector(".imagemErrada.nao-preenchida").value
-    const respostaIncorreta = document.querySelector(".respostaIncorreta.nao-preenchida").value;
+	
+	for(let k = 0; k < totalDePerguntas; k++){
+		if(textoPergunta[k].value.length < 20 || !isImage(imageC[k].value) || 
+			respostaCorreta[k].value === '' || (respostaIncorreta1[k].value === '' &&
+			respostaIncorreta2[k].value === '' && respostaIncorreta3[k].value === '')){
+        		conditionsNotMet = true;
+    	}
+	}
 
-    if(textoPergunta.length < 20 || respostaCorreta === '' || !isImage(imageC)
-    || respostaIncorreta === '' || !isImage(imageE)){
-        alert("Preencha os dados corretamente");
-    } 
-    
-    testar();
-
+	if(conditionsNotMet){
+		alert("Preencha os dados corretamente");
+	}else{
+		for(let i = 0; i < totalDePerguntas;i++){
+			myQuizz.questions[i].title = textoPergunta[i].value;
+			myQuizz.questions[i].color = corPergunta[i].value;
+			myQuizz.questions[i].answers = [
+				{
+					text: respostaCorreta[i].value,
+					image: imageC[i].value,
+					isCorrectAnswer: true
+				},
+				{
+					text: respostaIncorreta1[i].value,
+					image: imageE1[i].value,
+					isCorrectAnswer: false
+				},
+				{
+					text: respostaIncorreta2[i].value,
+					image: imageE2[i].value,
+					isCorrectAnswer: false
+				},
+				{
+					text: respostaIncorreta3[i].value,
+					image: imageE3[i].value,
+					isCorrectAnswer: false
+				},
+			];
+		}
+		document.querySelector(".pagequizz_2").classList.toggle("hidden");
+		document.querySelector(".pagequizz_3").classList.toggle("hidden");
+	}		
 }
 
-function testar(){
-    console.log(respostaIncorreta)
+function select(elemento){
+const perguntaSelecionada = document.querySelector(".content.selecionado");
+	
+  if(perguntaSelecionada !== null){
+    perguntaSelecionada.classList.toggle("selecionado");
+  }
+  elemento.parentNode.parentNode.classList.toggle("selecionado");
 }
